b5826cfc4a0cac0abc4ea23361f36fc288439b05
odiegosilva1 <odiegosilvadev@gmail.com>
2025-07-25 11:10:29 -0300

    update:main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ db.connect()
 
 db.create_tables([Usuario, Anuncio])
 
+usuario = Usuario.create(nome="jocalino", email="joca@email.com", senha="666555")
+usuario = Usuario.create(nome="mariula", email="mariula@email.com", senha="666555")
+usuario = Usuario.create(nome="zoio", email="zoio@email.com", senha="666555")
+
+print("Novo usuário:", usuario.id, usuario.nome, usuario.email)
+
