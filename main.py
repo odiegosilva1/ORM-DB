56f6f9fbebb7bbc71226c46e96c1ff660dda7b75
odiegosilva1 <odiegosilvadev@gmail.com>
2025-07-28 16:51:50 -0300

    add:flask

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from databese import db, Usuario, Anuncio
-
-#Call Function
-db.connect()
-
-db.create_tables([Usuario, Anuncio])
-
-usuario = Usuario.create(nome="jocalino", email="joca@email.com", senha="666555")
-usuario = Usuario.create(nome="mariula", email="mariula@email.com", senha="666555")
-usuario = Usuario.create(nome="zoio", email="zoio@email.com", senha="666555")
-
-print("Novo usuário:", usuario.id, usuario.nome, usuario.email)
-
+from flask import Flask
